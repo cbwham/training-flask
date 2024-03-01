483e30d7e1884fe2053abf1693a13ca1c91dea65
gretel <code@jitter.eu>
2024-03-01 11:07:06 +0100

    draft implementation for #1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route("/")
 def hallo():
@@ -11,3 +13,46 @@ def hallo():
 @app.route("/feierabend")
 def fa():
     return "11 uhr 15"
+
+@app.route('/colors/<palette>/')
+def colors(palette):
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    all_colors = {
+        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
+
+app.run(debug=True)
