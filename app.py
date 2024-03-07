52c1517fb4236250fa848bb10d7eb96d3aad79dc
gretel <code@jitter.eu>
2024-03-07 15:24:42 +0100

    tidy cleanup

--- app.py
+++ app.py
@@ -2,58 +2,15 @@
 
 from flask import Flask, jsonify
 from healthcheck import HealthCheck
-#from flasgger import Swagger
 
 app = Flask(__name__)
-#swagger = Swagger(app)
 health = HealthCheck()
 
 # Add a flask route to expose information
 app.add_url_rule('/healthcheck', 'healthcheck', view_func=lambda: health.run())
 
 @app.route("/")
-def hallo():
-    return "yo"
-
-@app.route('/colors/<palette>/')
-def colors(palette):
-    """Example endpoint returning a list of colors by palette
-    This is using docstrings for specifications.
-    ---
-    parameters:
-      - name: palette
-        in: path
-        type: string
-        enum: ['all', 'rgb', 'cmyk']
-        required: true
-        default: all
-    definitions:
-      Palette:
-        type: object
-        properties:
-          palette_name:
-            type: array
-            items:
-              $ref: '#/definitions/Color'
-      Color:
-        type: string
-    responses:
-      200:
-        description: A list of colors (may be filtered by palette)
-        schema:
-          $ref: '#/definitions/Palette'
-        examples:
-          rgb: ['red', 'green', 'blue']
-    """
-    all_colors = {
-        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
-        'rgb': ['red', 'green', 'blue']
-    }
-    if palette == 'all':
-        result = all_colors
-    else:
-        result = {palette: all_colors.get(palette)}
-
-    return jsonify(result)
+def index():
+    return '<!doctype html><html lang=en><head><meta charset=utf-8><title>CBW</title></head><body><p>FSH35!</p></body></html>'
 
 app.run()
