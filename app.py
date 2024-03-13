6b268821719abc1248ef2f55ac7c5a7809f648ec
gretel <code@jitter.eu>
2024-03-13 11:20:02 +0100

    add a route to check if there is break time or not

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask
 from healthcheck import HealthCheck
 
 app = Flask(__name__)
@@ -8,6 +8,22 @@ health = HealthCheck()
 
 # Add a flask route to expose information
 app.add_url_rule('/healthcheck', 'healthcheck', view_func=lambda: health.run())
+
+@app.route("/pause", methods=["GET"])
+def pause():
+    """
+    Returns True if the current time is in the following ranges: 9:30-9:45, 11:15-11:30
+    """
+    import datetime
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    now_cet = now.astimezone(tz=datetime.timezone(datetime.timedelta(hours=1)))
+    cond = str(
+        (
+            (9 <= now_cet.hour <= 9 and 30 <= now_cet.minute <= 45)
+            or (11 <= now_cet.hour <= 11 and 15 <= now_cet.minute <= 30)
+        )
+    )
+    return {"pause": cond}
 
 @app.route("/")
 def index():
