a9e7270ade86da28f4891214f82bc1115f6b5a1d
gretel <code@jitter.eu>
2024-03-14 10:30:22 +0100

    conformity fixes using pylint

--- app.py
+++ app.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
 
+"""Module providing a basic example on the use of Flask."""
+
+import datetime
 from flask import Flask
 from healthcheck import HealthCheck
-import datetime
 
 app = Flask(__name__)
 health = HealthCheck()
 
 # Add a flask route to expose information
-app.add_url_rule('/healthcheck', 'healthcheck', view_func=lambda: health.run())
+app.add_url_rule('/healthcheck', 'healthcheck', view_func=health.run())
 
 
 # "index"
 @app.route("/")
 def index():
-  return '<!doctype html><html lang=en><head><meta charset=utf-8><title>CBW</title></head><body><p>FSH35!</p></body></html>'  # string
+    return '<!doctype html><html lang=en><head><meta charset=utf-8><title>CBW</title></head><body><p>FSH35!</p></body></html>'  # string
 
 
 # dynamic content
 @app.route('/name/<name>')
 def show(name):
-  return f'<!doctype html><html lang=en><head><meta charset=utf-8><title>CBW - Azure Wars</title></head><body ><p>Greetings, Master {name}!</p></body></html>'  # string
+    return f'<!doctype html><html lang=en><head><meta charset=utf-8><title>CBW - Azure Wars</title></head><body ><p>Greetings, Master {name}!</p></body></html>'  # string
 
 
 # happy day?
 def is_friday(dt):
-  return dt.weekday() == 4  # 0=monday
+    return dt.weekday() == 4  # 0=monday
 
 
 @app.route("/pause")
 def pause():
-  """
+    """
     Returns True if the current time is in the following ranges: 9:30-9:45, 11:15-11:30, 12:15-12:45 on Fridays
     """
-  # current time
-  now = datetime.datetime.now(tz=datetime.timezone.utc)
-  # .. in central europe
-  now_cet = now.astimezone(tz=datetime.timezone(datetime.timedelta(hours=1)))
-  # DEBUG
-  print(repr(now_cet))
+    # current time
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    # .. in central europe
+    now_cet = now.astimezone(tz=datetime.timezone(datetime.timedelta(hours=1)))
+    # DEBUG
+    print(repr(now_cet))
 
-  # define condition of classroom pauses
-  cond = bool((9 <= now_cet.hour <= 9 and 30 <= now_cet.minute <= 45)
-              or (11 <= now_cet.hour <= 11 and 15 <= now_cet.minute <= 30)
-              or (12 <= now_cet.hour <= 15 and 12 <= now_cet.minute <= 45
-                  and is_friday(now_cet)))
-  return {"datetime": now_cet, "pause": cond}  # json
+    # define condition of classroom pauses
+    cond = bool((9 <= now_cet.hour <= 9 and 30 <= now_cet.minute <= 45)
+        or (11 <= now_cet.hour <= 11 and 15 <= now_cet.minute <= 30)
+        or (12 <= now_cet.hour <= 15 and 12 <= now_cet.minute <= 45
+            and is_friday(now_cet)))
+    return {"datetime": now_cet, "pause": cond}  # json
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
